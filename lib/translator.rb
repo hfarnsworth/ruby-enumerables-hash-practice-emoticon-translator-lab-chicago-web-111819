588e44df6ebec9225acbf59b806547225829f331
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-14 10:30:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,7 @@
 require "yaml"
 
 def load_library(file)
-  pp YAML.load_file(file).map {
-    |k,v| [:get_meaning=> (v[1]=> k ), :get_emoticon=> (v[0]=> v[1] )];
-    
-  }
-  
+  pp YAML.load_file(file).map { |k,v| [:get_meaning=> (v[1]=> k ), :get_emoticon=> (v[0]=> v[1] )] }
 end
 
 def get_japanese_emoticon
