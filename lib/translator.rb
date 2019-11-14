e23d16676f7f2c519e5e5aa0a0eda87bb2ed308f
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-14 10:41:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,13 @@ def load_library(file)
   pp data_hash = YAML.load_file(file)
   
   get_meaning = data_hash.map { |k,v| [v[1], k ] }.to_h
+  get_emotion = dat_hash.map { |k,v| [v[0], v[1] ] }.to_h
   
-  get_meaning
+  new_data = Hash.new
+  
+  new_data[:get_meaning] = get_meaning
+  new_data[:get_emoticon] = get_emoticon
+  new_data
 end
 
 def get_japanese_emoticon
